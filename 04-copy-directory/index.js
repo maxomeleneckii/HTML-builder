1a3dac400afad647c91e231a4105f9ab8fc99f47
Max <max.omeleneckii@gmail.com>
2021-11-07 15:08:09 +0300

    feat: added js cod in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const oldPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
 
+fs.mkdir(newPath, {
+  recursive: true
+}, err => {
+  if (err) throw err;
+});
+fs.readdir(newPath, (err, files) => {
+  for (let i = 0; i < files.length; i++) {
+    fs.unlink(newPath + '/' + files[i], err => {
+      if (err) throw err;
+    });
+  }
+});
+fs.readdir(oldPath, (err, files) => {
+  if (err) throw err;
+  for (let i = 0; i < files.length; i++) {
+    fs.copyFile(oldPath + '/' + files[i], newPath + '/' + files[i], err => {
+      if (err) throw err;
+    });
+  }
+});
