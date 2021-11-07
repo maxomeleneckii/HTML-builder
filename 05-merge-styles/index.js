c38afcfdf808e21b5e84ecd41bb7eb3fec3803bf
Max <max.omeleneckii@gmail.com>
2021-11-07 22:35:17 +0300

    refactor: deleted console.log

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -5,10 +5,8 @@ const projectPath = path.join(__dirname, 'project-dist');
 const output = fs.createWriteStream(path.join(projectPath, 'bundle.css'));
 fs.readdir(stylesPath, (err, files) => {
   if (err) throw err;
-  console.log('Files:\n' + files);
   for (let i = 0; i < files.length; i++) {
     let extension = path.extname(files[i]).split('.').pop();
-    console.log(extension);
     if (extension === 'css') {
       const input = fs.createReadStream(path.join(stylesPath, files[i]));
       input.on('data', data => {
